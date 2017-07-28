fb6138afd3a42b01e01b8001b92eba431525bda4
Carlos García <deivicarlos@gmail.com>
2017-07-28 18:54:53 -0400

    fixed issues

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var app = express();
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname+'index.html');
+	res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/LocalWeatherApp', function(req, res){
+	res.sendFile(__dirname+'/LocalWeatherApp/index.html');
 });
  
 app.listen(process.env.PORT || 3000, function(){
