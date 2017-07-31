f080758bbd07113e3c1bd3c1a2402ecbf3034b5d
Carlos García <deivicarlos@gmail.com>
2017-07-31 19:20:39 -0400

    updates

--- LocalWeatherApp/js/test.js
+++ LocalWeatherApp/js/test.js
@@ -5,7 +5,7 @@ const googeApiKey = "AIzaSyDQV1m-AHKnntgP3s8jcvZMNCbbyawyeEw";
 const DarkSkyKey = "d22ee70a5d19dfc81bf5eac09e8d6e51";
 
 var DarkSkyRequest = "https://api.darksky.net/forecast/" + DarkSkyKey + "/";
-const mapsUrl = "https://maps.googleapis.com/maps/api/geocode/json";
+const mapsUrl = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false";
 
 var currentLocation = {
   lng: "",
@@ -19,34 +19,31 @@ $(document).ready(function(){
   
     geocoder = new google.maps.Geocoder();
     getLocation();
-    //getCityName();
+    
 
-    $("#btnGetLocation").click(getCityName);
+   // $("#btnGetLocation").click(getCityName);
   
 });
 
 //http://maps.googleapis.com/maps/api/geocode/json?latlng=33.7532358,-117.7901088&sensor=false
 
 
-
 function getCityName(){
 
   var latitudeLongitude = currentLocation.lat + "," + currentLocation.lng;
 
-  alert(latitudeLongitude);
-  alert(mapsUrl);
-  $.getJSON( mapsUrl, {
-    latlng: latitudeLongitude,
-    key: googeApiKey  
-  },
-  function(results){
-      console.log(results);
-
-  });
-        
-      
+  if(currentLocation.lat && currentLocation.lng){
+    alert(latitudeLongitude);
+    alert(mapsUrl);
   
-
+    $.getJSON( mapsUrl, {
+    latlng: latitudeLongitude
+    //key: googeApiKey  
+    },
+    function(results){
+      console.log(results);
+    });
+  }
 }
 
 
@@ -73,18 +70,18 @@ function getTemperature(){
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(savePosition);
+        getCityName();
     } else {
         alert("Geolocation is not supported by this browser.");
     }
 }
 
 function savePosition(position) {
-    currentLocation.lat = position.coords.latitude;
-    currentLocation.lng = position.coords.longitude; 
 
-    $("#location").html("Latitude: " + currentLocation.lat + " Longitude: " + currentLocation.lng + " Ciudad: "+ currentLocation.city + " Pais: " );
-
-    //codeLatLng(currentLocation.latitude, currentLocation.longitude);
+  currentLocation.lat = position.coords.latitude;
+  currentLocation.lng = position.coords.longitude;
+  
+  $("#location").html("Latitude: " + currentLocation.lat + " Longitude: " + currentLocation.lng + " Ciudad: "+ currentLocation.city + " Pais: " );
 }
 
 
